c670182cb5fe973cadae4b599b666c150bd4c57e
anon767 <tomganzka@gmail.com>
2018-01-25 09:08:54 +0100

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -14,15 +14,10 @@ pub struct ConfigDocument {
 }
 
 pub fn parse_config(content: &String) -> Result<(ConfigDocument), Error> {
-    // Some JSON input data as a &str. Maybe this comes from the user.
     let data = content;
 
-    // Parse the string of data into a Person object. This is exactly the
-    // same function as the one that produced serde_json::Value above, but
-    // now we are asking it for a Person as output.
     let config: ConfigDocument = serde_json::from_str(data)?;
 
-    // Do things just like with any other Rust data structure.
     println!("loading Shit with ip {}:{} \n {} threads \n at {} ", config.ip, config.port, config.port, config.document_root);
 
     Ok(config)
