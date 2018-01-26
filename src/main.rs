f494664f23657a820a17d9487302e2ee84f0bdee
anon767 <tomganzka@gmail.com>
2018-01-26 13:20:02 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use self::tiny_http::{Response, Request};
 fn main() {
 
     let config_path: String = "/home/tom/config.json".to_string();
-    let config: ConfigDocument = parse_config(&config_path);
+    let config: ConfigDocument = parse_file(&config_path);
     let pool = ThreadPool::new(config.threads);
     let webServer = server::WebServer;
     webServer.run(pool,config);
